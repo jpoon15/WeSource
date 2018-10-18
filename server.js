026332cd61bded9fb3e40ad6c755627ee8f008ce
Jenny Poon <jenny.poon@live.ca>
2018-10-18 23:14:32 +0000

    created routes for homepage, resources and users

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
+const homepageRoutes = require("./routes/homepage");
 const usersRoutes = require("./routes/users");
 const resourcesRoutes = require("./routes/resources");
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -36,6 +38,7 @@ app.use("/styles", sass({
 app.use(express.static("public"));
 
 // Mount all resource routes
+app.use("/api/homepage", homepageRoutes(knex));
 app.use("/api/users", usersRoutes(knex));
 app.use("/api/resources", resourcesRoutes(knex));
 
