f8c031082c5421cce298b57099660a7e77d5c904
Jenny Poon <jenny.poon@live.ca>
2018-10-18 22:17:33 +0000

    updated href tag so path is accurate

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ $(() => {
     url: "/api/users"
   }).done((resources) => {
     for(resource of resources) {
-      $("<a>").attr("href", `/api/users/search/${resource.id}`).text(resource.title).appendTo($("body"));
+      $("<a>").attr("href", `/api/resources/${resource.id}`).text(resource.title).appendTo($("body"));
     }
   });
 
