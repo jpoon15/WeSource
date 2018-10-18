af37434342006ddababb6058a2d435778cc6bb87
Jenny Poon <jenny.poon@live.ca>
2018-10-18 23:16:49 +0000

    edit url reference section to homepage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/users"
+    url: "/api/homepage"
   }).done((resources) => {
     for(resource of resources) {
       $("<a>").attr("href", `/api/resources/${resource.id}`).text(resource.title).appendTo($("body"));
