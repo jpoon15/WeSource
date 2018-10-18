ef34fb0075a9fb50e5165b5d99e6dfbcb1c55bf3
Jenny Poon <jenny.poon@live.ca>
2018-10-18 20:27:52 +0000

    deleted unused codes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,20 +2,9 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+  }).done((resources) => {
+    for(resource of resources) {
+      $("<a>").attr("href", `/api/users/resources/${resource.id}`).text(resource.title).appendTo($("body"));
     }
   });;
-
-  $.ajax({
-    method: "GET",
-    url: "/api/users/resources"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-
-
 });
