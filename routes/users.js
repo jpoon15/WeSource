89e78b81d19a0686d006e67321a875f8b5c4b794
Jenny Poon <jenny.poon@live.ca>
2018-10-18 03:14:30 +0000

    created route to query database for search keyword

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,6 @@ const router  = express.Router();
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
-    console.log(req.params)
     knex
       .select("*")
       .from("resources")
@@ -17,16 +16,48 @@ module.exports = (knex) => {
 
   router.get("/resources", (req, res) => {
     let searchKeyword = req.query.search
-    knex
-      .select("*")
+    console.log("search", req.query.search);
+    /////
+
+ //      knex("dept").where("deptno","50").del()
+ // .then(function (count) {
+ //   console.log(count);
+ // })
+ // .finally(function () {
+ //   knex.destroy();
+ // });
+
+
+
+ ///Select query
+ // knex.select("*").from("resources")
+ // .where('title')
+ // .then(function (depts){
+ //   depts.forEach((dept)=>{ //use of Arrow Function
+ //     console.log({...dept});
+ //   });
+ // }).catch(function(err) {
+ // // All the error can be checked in this piece of code
+ //   console.log(err);
+ // }).finally(function() {
+ //   // To close the connection pool
+ //   knex.destroy();
+ // });
+
+    /////
+
+    knex.select("*")
       .from("resources")
-      .where("title").like(`%${searchKeyword}%`)
-      .or("description").like(`%${searchKeyword}%`)
-      .or("link").link(`%${searchKeyword}%`)
+      .where("title", "like",`%${searchKeyword}%`)
+      // .or("description","like",`%${searchKeyword}%`)
+      // .or("link", "like",`%${searchKeyword}%`)
       .then((results) => {
+        console.log("we are in the results section");
         res.json(results);
       });
   });
+
+   //query.where('company', 'like', '%marker%');
 
   return router;
 }
