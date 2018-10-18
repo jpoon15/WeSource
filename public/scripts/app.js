43bb73e0de09e80d0f3fa3ae5ea1b895f8aeb5d2
toonflash <toonflash@hotmail.com>
2018-10-18 22:57:24 +0000

    fixed empty data object being returned to front end, started populating card area with results

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,39 @@
 $(() => {
+  // on load
   $.ajax({
     method: "GET",
     url: "/api/users"
   }).done((resources) => {
     for(resource of resources) {
-      $("<a>").attr("href", `/api/users/resources/${resource.id}`).text(resource.title).appendTo($("body"));
+      $("<a>").attr("href", `/api/users/search/${resource.id}`).text(resource.title).appendTo($("body"));
     }
-  });;
+  });
+
+  // Search Bar Query
+  $("#searchButton").on("click", (e) => {
+    e.preventDefault();
+    // set search query as let
+    let searchKeyword = $('#searchKeyword').val();
+    $.ajax({
+      // route to GET from  
+      url: "/api/users/search",
+      method: "GET",
+      data: {
+        search: searchKeyword
+      },
+      dataType: "json",
+      success: function(response){
+        $('.card-columns').empty();
+        response.forEach(item => {
+          console.log(item);
+          $(`<div class="card"><p>${item.title}</p><p>${item.description}</p><a href="${item.link}">${item.link}</a><p>${item.category_id}</p></div>`).prependTo($('.card-columns'));
+        })
+    },
+      error: function(err){
+        // do error stuff
+      }
+    });
+  });
+
+
 });
--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,6 @@
 const express = require('express');
 const router  = express.Router();
 
-function searchResourceFilter(keyword) {
-  knex.select("*")
-    .from("resources")
-    .where("title", "like",`%${keyword}%`)
-    .orWhere("description","like",`%${keyword}%`)
-    .orWhere("link", "like",`%${keyword}%`)
-    .then((results) => {
-      res.json(results);
-    })
-}
-
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
@@ -27,9 +16,14 @@ module.exports = (knex) => {
 
   router.get("/search", (req, res) => {
     let searchKeyword = req.query.search;
-    console.log("search", req.query.search);
-    searchResourceFilter(searchKeyword);
-    res.redirect("/");
+    knex.select("*")
+    .from("resources")
+    .where("title", "like",`%${searchKeyword}%`)
+    .orWhere("description","like",`%${searchKeyword}%`)
+    .orWhere("link", "like",`%${searchKeyword}%`)
+    .then((results) => {
+      res.json(results);
+    })
   });
 
   return router;
