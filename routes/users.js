3464dd887405d90439dfb9ae4358b69031a64329
Jenny Poon <jenny.poon@live.ca>
2018-10-18 20:28:40 +0000

    search query function modularized

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,17 @@
 const express = require('express');
 const router  = express.Router();
 
+function searchResourceFilter(keyword) {
+  knex.select("*")
+    .from("resources")
+    .where("title", "like",`%${keyword}%`)
+    .orWhere("description","like",`%${keyword}%`)
+    .orWhere("link", "like",`%${keyword}%`)
+    .then((results) => {
+      res.json(results);
+    })
+}
+
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
@@ -14,50 +25,12 @@ module.exports = (knex) => {
     });
   });
 
-  router.get("/resources", (req, res) => {
-    let searchKeyword = req.query.search
+  router.get("/search", (req, res) => {
+    let searchKeyword = req.query.search;
     console.log("search", req.query.search);
-    /////
-
- //      knex("dept").where("deptno","50").del()
- // .then(function (count) {
- //   console.log(count);
- // })
- // .finally(function () {
- //   knex.destroy();
- // });
-
-
-
- ///Select query
- // knex.select("*").from("resources")
- // .where('title')
- // .then(function (depts){
- //   depts.forEach((dept)=>{ //use of Arrow Function
- //     console.log({...dept});
- //   });
- // }).catch(function(err) {
- // // All the error can be checked in this piece of code
- //   console.log(err);
- // }).finally(function() {
- //   // To close the connection pool
- //   knex.destroy();
- // });
-
-    /////
-
-    knex.select("*")
-      .from("resources")
-      .where("title", "like",`%${searchKeyword}%`)
-      // .or("description","like",`%${searchKeyword}%`)
-      // .or("link", "like",`%${searchKeyword}%`)
-      .then((results) => {
-        console.log("we are in the results section");
-        res.json(results);
-      });
+    searchResourceFilter(searchKeyword);
+    res.redirect("/");
   });
-
-   //query.where('company', 'like', '%marker%');
 
   return router;
 }
