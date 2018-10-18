06dbf863a770ffdab82f273ccbecda0cd99dec6d
Jenny Poon <jenny.poon@live.ca>
2018-10-18 02:39:31 +0000

    created get route for /resources to find keyword in db

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,26 @@ const router  = express.Router();
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
+    console.log(req.params)
     knex
       .select("*")
-      .from("users")
+      .from("resources")
       .then((results) => {
         res.json(results);
     });
+  });
+
+  router.get("/resources", (req, res) => {
+    let searchKeyword = req.query.search
+    knex
+      .select("*")
+      .from("resources")
+      .where("title").like(`%${searchKeyword}%`)
+      .or("description").like(`%${searchKeyword}%`)
+      .or("link").link(`%${searchKeyword}%`)
+      .then((results) => {
+        res.json(results);
+      });
   });
 
   return router;
