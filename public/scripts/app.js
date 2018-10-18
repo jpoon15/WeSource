b13b654075ee2ce1cf74b8169674a07afb2f70ee
Jenny Poon <jenny.poon@live.ca>
2018-10-18 03:14:14 +0000

    created route to query database for search keyword

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,4 +7,15 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+
+  $.ajax({
+    method: "GET",
+    url: "/api/users/resources"
+  }).done((users) => {
+    for(user of users) {
+      $("<div>").text(user.name).appendTo($("body"));
+    }
+  });;
+
+
 });
