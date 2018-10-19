81a5ab52812e02c0d96d094db49eb23ed1bb2486
toonflash <toonflash@hotmail.com>
2018-10-19 19:10:08 +0000

    commiting after re-working howthe routes are set up with knex and added an imgurl to our resources database and seeded with img urls

--- db/seeds/03_resources.js
+++ db/seeds/03_resources.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('resources').insert({title: 'Funny Cat meme', description: 'Cat fails', link:'www.google.com', category_id: 1, user_id:1 }),
-        knex('resources').insert({title: 'Technology Rocks!', description:'21st century, ERA of Technology', link:'www.dribbble.com', category_id: 2, user_id: 2 }),
-        knex('resources').insert({title: 'Web-design fundamentals', description:'Important design fundamentals', link:'www.behance.com', category_id:3, user_id: 3 })
+        knex('resources').insert({title: 'Funny Cat meme', description: 'Cat fails', link:'http://www.google.com', category_id: 1, user_id:1 }),
+        knex('resources').insert({title: 'Technology Rocks!', description:'21st century, ERA of Technology', link:'http://www.dribbble.com', category_id: 2, user_id: 2 }),
+        knex('resources').insert({title: 'Web-design fundamentals', description:'Important design fundamentals', link:'http://www.behance.com', category_id:3, user_id: 3 })
       ]);
     });
 };
--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,11 @@
 
 const express = require('express');
 const router  = express.Router();
+const knex    = require('../lib/database-connection');
 
-module.exports = (knex) => {
+module.exports = router;
 
-  router.get("/:id", (req, res) => {
-    res.render("dashboard");
+router.get("/:id", (req, res) => {
+  res.render("dashboard");
+});
 
-  });
-  return router;
-}
