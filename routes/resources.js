a0c5f50e1a374fce1ab8d87a53ad9e0f9bae54bd
Jenny Poon <jenny.poon@live.ca>
2018-10-19 19:10:29 +0000

    added route for search, want to move from homepage to resource routes

--- routes/resources.js
+++ routes/resources.js
@@ -27,3 +27,23 @@ module.exports = (knex) => {
 
   return router;
 }
+
+  router.get("/new", (req, res) => {
+    res.render("detail_page");
+
+  });
+
+  router.get("/search", (req, res) => {
+    let searchKeyword = req.query.search;
+    console.log("search", req.query.search);
+
+    knex.select("*")
+      .from("resources")
+      .where("title", "like",`%${searchKeyword}%`)
+      .orWhere("description","like",`%${searchKeyword}%`)
+      .orWhere("link", "like",`%${searchKeyword}%`)
+      .then((results) => {
+        res.json(results);
+      })
+      res.redirect("/")
+  });
