2ffed5f33424aed3d1eaa8c27b7e0ba39af7ed5f
Tuguldur <tuguldurbold5@gmail.com>
2018-10-20 00:38:26 +0000

    added and modified ejs and users.js files

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,17 @@ const knex    = require('../lib/database-connection');
 module.exports = router;
 
 router.get("/:id", (req, res) => {
+      knex
+      .select("*")
+      .from("resources")
+      .join('categories', 'resources.category_id', '=', 'categories.id')
+      .select('resources')
+      .where({user_id: '1'})
+      .then((results) => {
+        console.log(results);
+        let templateVars= {articles: results}
+        res.render("mydashboard", templateVars);
+      });
   res.render("mydashboard");
 });
 
