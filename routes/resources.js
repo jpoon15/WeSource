bf50db2318cba6ff1c50e511b59e58bd85ceb583
Jenny Poon <jenny.poon@live.ca>
2018-10-20 18:30:53 +0000

    created entry in db when like resource

--- routes/resources.js
+++ routes/resources.js
@@ -6,6 +6,7 @@ const knex    = require('../lib/database-connection');
 
 module.exports = router;
 
+//DISPLAY SHOW PAGE
 router.get("/:id", (req, res) => {
   console.log(req.params.id)
   let currentResourceId = req.params.id
@@ -25,6 +26,7 @@ router.get("/:id", (req, res) => {
 router.post("/add", (req, res) => {
   let userCurrent = req.session.id;
   console.log("we are in the add post", req.body);
+
   knex('resources').insert({
       link: req.body.link,
       title: req.body.title,
@@ -34,6 +36,22 @@ router.post("/add", (req, res) => {
   }).returning('id')
   .then((id) => {
     console.log("successfully inserted the record ");
+    console.log(id);
+    res.json({result: "True"});
+  })
+})
+
+//LIKING RESOURCE
+router.post("/like", (req, res) => {
+  let userCurrent = req.session.id
+  console.log("we are in the like resource", req.body);
+
+  knex('likes').insert({
+    user_id: userCurrent,
+    resource_id: req.body.resource_id
+  }).returning('id')
+  .then((id) => {
+    console.log("successfully inserted the record");
     console.log(id);
     res.json({result: "True"});
   })
