84e199f6d2e966b596f61151d22ce01b0e1b381c
toonflash <toonflash@hotmail.com>
2018-10-20 01:45:59 +0000

    need to do a pull

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,8 +4,11 @@ $(() => {
     method: "GET",
     url: "/api/homepage"
   }).done((resources) => {
+    console.log(resources);
     for(resource of resources) {
-      $("<a>").attr("href", `/api/resources/${resource.id}`).text(resource.title).appendTo($("body"));
+      // $("<a>").attr("href", `/api/resources/${resource.id}`).text(resource.title).appendTo($("body"));
+      //$(`<a href="/api/resources/${resource.id}"><div class="card card-pin"><img class="card-img" src="${resource.imgurl}"/><p>${resource.title}</p><p>${resource.description}</p><a href="${resource.link}">${resource.link}</a><p>${resource.category_id}</p></div></a>`).prependTo($('.card-columns'));
+      $(`<a href="/api/resources/${resource.id}"><div class="card card-pin"><img class="card-img" src="${resource.imgurl}"/><p>${resource.title}</p><p>${resource.description}</p><p>${resource.category_id}</p></div></a>`).prependTo($('.card-columns'));
     }
   });
 
@@ -50,5 +53,36 @@ $(() => {
     $('body').removeClass('fixed');
     $('#addResourceModal').hide();
   })
+
+  $('#addResource').on('click', (e) => {
+    e.preventDefault();
+
+    var category_id = $('#addResourceModal #category').find(':selected').val();
+
+    var data  = {
+      link: $('#url').val(),
+      title: $('#title').val(),
+      description: $('#description').val(),
+      category_id: category_id
+    };
+    //ajax call to save data 
+    console.log("before ajax request ",data);
+    $.ajax({
+      url: '/api/resources/add',
+      data: data,
+      type:'POST',
+      success: function(result){
+        console.log("we are in success");
+        // add success div notice
+        let newPost = $(`<div class="card card-pin"><img class="card-img" src="${result.imgurl}"/><p>${result.title}</p><p>${result.description}</p><a href="${result.link}">${result.link}</a><p>${result.category_id}</p></div>`);
+        $(newPost).prependTo($('.card-columns'))
+      },
+      error: function(error){
+        console.log("we are in error");
+      }
+    });
+  })
+
+  //function insert
 
 });
--- routes/resources.js
+++ routes/resources.js
@@ -8,7 +8,6 @@ module.exports = router;
 
 router.get("/:id", (req, res) => {
   res.render("detail");
-
 });
 
 router.get("/search", (req, res) => {
@@ -25,3 +24,33 @@ router.get("/search", (req, res) => {
     })
     res.redirect("/")
 });
+
+router.post("/add", (req, res) => {
+  let userCurrent = req.session.id;
+  console.log("we are in the add post", req.body);
+  //let newData = req.body;
+  knex('resources').insert({
+      link: req.body.link,
+      title: req.body.title,
+      description: req.body.description,
+      category_id: req.body.category_id,
+      user_id: userCurrent
+  }).returning('id')
+  .then((id) => {                                                                                                                                                                                                               
+    console.log("successfully inserted the record ");
+    console.log(id);
+    res.json({result: "True"});
+  })
+  //let userCurrent = req.session.id;
+  // //console.log("REQ cat: ", req.body);
+  // let newResource = {
+  //   link: req.body.resourceUrl,
+  //   title: req.body.resourceTitle,
+  //   description: req.body.resourceDescription,
+  //   category_id: req.body.resourceCategory, 
+  //   //imgUrl = req.body.resourceCategory,
+  //   user_id: userCurrent
+  // }
+
+  //console.log("NEW RESOURCE: ", newResource);
+})
