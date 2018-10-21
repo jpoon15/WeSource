6aec0da9f411978a2c7ae9b714a5ab2e9a3e1a51
Tuguldur <tuguldurbold5@gmail.com>
2018-10-21 01:17:43 +0000

    modified partials

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,35 @@ const bcrypt  = require('bcryptjs');
 module.exports = router;
 
 router.get("/:id", (req, res) => {
+<<<<<<< HEAD
+  
+  knex.select(
+    'r.id',
+    'r.title',
+    'r.link',
+    'r.description',
+    'r.imgurl',
+    'r.category_id',
+    'r.user_id',
+    'cs.id',
+    'cs.category',
+    'lks.id as likesId',
+    'lks.resource_id as likesResourceId ',
+    'lks.user_id as likesUserId',
+    'usr.email'
+    )
+    .from('resources AS r')
+    .leftJoin('categories AS cs', 'r.category_id', 'cs.id') //joining resources with categories 
+    .leftJoin('likes AS lks', 'r.id', 'lks.resource_id')
+    .leftJoin('users AS usr', 'r.user_id', 'usr.id' )
+    .where('lks.user_id', '=', 1)
+    .then((results)=>{
+      console.log(results)
+      let templateVars= {articles: results}
+        res.render("mydashboard", templateVars);
+    });
+
+=======
   knex
     .select("*")
     .from("resources")
@@ -19,6 +48,7 @@ router.get("/:id", (req, res) => {
       let templateVars= {articles: results}
       res.render("mydashboard", templateVars);
     })
+>>>>>>> master
 })
 
 router.get("/:id/profile", (req, res) => {
