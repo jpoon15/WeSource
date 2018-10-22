ee2e622723269128c6feecda46635d4409def555
Jenny Poon <jenny.poon@live.ca>
2018-10-22 04:53:55 +0000

    added some real hard coded data for database

--- db/seeds/04_likes.js
+++ db/seeds/04_likes.js
@@ -2,8 +2,8 @@
 exports.seed = function(knex, Promise) {
   return Promise.all([
     // Inserts seed entries
-    knex('likes').insert({resource_id: 1, user_id: 1}),
+    knex('likes').insert({resource_id: 1, user_id: 3}),
     knex('likes').insert({resource_id: 2, user_id: 2}),
-    knex('likes').insert({resource_id: 3, user_id: 3}),
+    knex('likes').insert({resource_id: 3, user_id: 1}),
   ]);
 };
