b3451143e324db719b3b0c84339b0a081737ae39
Jenny Poon <jenny.poon@live.ca>
2018-10-22 01:34:19 +0000

    renaming of incoming data to prevent overwriting

--- routes/homepage.js
+++ routes/homepage.js
@@ -11,8 +11,18 @@ router.get("/", (req, res) => {
   knex("resources")
     .where('delete', 0)
     .join("categories", "categories.id", "=", "resources.category_id")
+    .select(
+      "resources.id as resources_id",
+      "resources.title as resources_title",
+      "resources.link as resources_link",
+      "resources.category_id as resources_category_id",
+      "resources.user_id as resources_user_id",
+      "resources.imgurl as resources_imgurl",
+      "resources.description as resources_description",
+      "categories.id as category_id",
+      "categories.category as categories_category"
+      )
     .then((results) => {
-      console.log("homepage results", results)
       res.json(results);
   });
 });
