9a5ed4e492a855b89b70a374d1b17ac6f5f5b048
Jenny Poon <jenny.poon@live.ca>
2018-10-22 03:09:42 +0000

    no changes, just merging branch to master

--- routes/homepage.js
+++ routes/homepage.js
@@ -32,26 +32,29 @@ router.get("/search", (req, res) => {
   let searchKeyword = (req.query.search).toLowerCase();
 
   knex("resources")
-    .where(
-      knex.raw('LOWER("title") like ?',`%${searchKeyword}%`))
-    .orWhere(
-      knex.raw('LOWER("description") like ?',`%${searchKeyword}%`))
-    .orWhere(
-      knex.raw('LOWER("link") like ?', `%${searchKeyword}%`))
-    .join("categories", "categories.id", "=", "resources.category_id")
-    .select(
-      "resources.id as resources_id",
-      "resources.title as resources_title",
-      "resources.link as resources_link",
-      "resources.category_id as resources_category_id",
-      "resources.user_id as resources_user_id",
-      "resources.imgurl as resources_imgurl",
-      "resources.description as resources_description",
-      "categories.id as category_id",
-      "categories.category as categories_category"
-      )
-    .then((results) => {
-      res.json(results);
-    })
-
+      .where(
+        knex.raw('LOWER("title") like ?',`%${searchKeyword}%`))
+      .orWhere(
+        knex.raw('LOWER("description") like ?',`%${searchKeyword}%`))
+      .orWhere(
+        knex.raw('LOWER("link") like ?', `%${searchKeyword}%`))
+      .groupBy("resources.id", "categories.id")
+      .having("delete", "=", 0)
+      .join("categories", "categories.id", "=", "resources.category_id")
+      .select(
+        "resources.id as resources_id",
+        "resources.title as resources_title",
+        "resources.link as resources_link",
+        "resources.category_id as resources_category_id",
+        "resources.user_id as resources_user_id",
+        "resources.imgurl as resources_imgurl",
+        "resources.description as resources_description",
+        "resources.delete as resources_delete",
+        "categories.id as category_id",
+        "categories.category as categories_category"
+        )
+      .then((results) => {
+        console.log("filtered results", results)
+        res.json(results);
+      })
 });
