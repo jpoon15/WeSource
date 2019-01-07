613517abcecc50bd211e3df5780ef93f0d7e3eef
Jenny Poon <jenny.poon@live.ca>
2019-01-07 15:52:01 -0800

    added port for production

--- knexfile.js
+++ knexfile.js
@@ -24,6 +24,8 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL + '?ssl=true',
+    port     : process.env.DB_PORT
+
     pool: {
       min: 2,
       max: 10
