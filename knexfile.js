f2f9ede24119a77c2c97e149bb5a7b92c7c00f4a
Jenny Poon <jenny.poon@live.ca>
2019-01-07 16:06:59 -0800

    removed port

--- knexfile.js
+++ knexfile.js
@@ -24,8 +24,6 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL + '?ssl=true',
-    port     : process.env.DB_PORT,
-
     pool: {
       min: 2,
       max: 10
